613815e0313fc29caad473d0bb626685fe109780
Canib-us <balev.danilll@gmail.com>
2025-06-10 00:08:48 +0700

    Initial commit

--- src/main/java/com/crm/CRM/CrmApplication.java
+++ src/main/java/com/crm/CRM/CrmApplication.java
@@ -10,6 +10,7 @@ public class CrmApplication {
 		SpringApplication.run(CrmApplication.class, args);
 		System.out.println("Hello World");
 		System.out.println("kalksmkac");
+		System.out.println("uiacebdc");
 	}
 
 }
